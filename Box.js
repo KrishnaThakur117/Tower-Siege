477d7fe26ea0198f0f54c32725c0cf7c1ad7c7c1
Krishna Thakur <71776030+KrishnaThakur117@users.noreply.github.com>
2020-09-29 15:11:11 +0400

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,6 +1,5 @@
-class Box extends BaseClass {
+class Box {
     constructor(x, y, width, height){
-      super(x,y,width,height);
-      }
+    }
   };
   
--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
+var Base,Block1,Block2,Block3,Block4,Block5,Block6;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  Base = new Ground(390, 205, 30, 120);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  Base.display();
+    
   drawSprites();
 }
